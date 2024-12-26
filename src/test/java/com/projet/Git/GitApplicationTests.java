597a564619a99fe311228e7d493fda547cf897c9
med amine <mademine13@gmail.com>
2024-12-26 19:22:10 +0100

    init commit

---
+++ src/test/java/com/projet/Git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.Git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
